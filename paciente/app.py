0b8254c3de94a4803b03debdba69119e39a36d14
CristianCaro915 <c.caro@uniandes.edu.co>
2023-12-05 12:02:16 -0500

    aaa

--- paciente/app.py
+++ paciente/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'postgresql+psycopg2://monitoring_user:isis2503@10.128.0.2:5432/monitoring_db'
+    'postgresql+psycopg2://pacientes_user:isis2503@10.128.0.3:5432/pacientes_db'
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
