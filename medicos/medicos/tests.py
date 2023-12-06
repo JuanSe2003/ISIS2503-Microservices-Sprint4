c1bf209dc76bd5bfa1da66ab7b3e3366f3493f4e
Juan Sebastian Pedraza Perez <js.pedrazap1@uniandes.edu.co>
2023-12-06 16:57:51 -0500

    Form

--- medicos/medicos/tests.py
+++ medicos/medicos/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django import forms
+from .models import Medico
+
+class MedicoForm(forms.ModelForm):
+    class Meta:
+        model = Medico
+        fields = [
+            'id',
+            'name',
+            'last_name',
+            'especialidad',
+            'consultorio'
+        ]
+        labels = {
+            'id': 'Id',
+            'name': 'Name',
+            'last_name': 'Last_name',
+            'especialidad': 'Especialidad',
+            'consultorio': 'Consultorio'
+        }
